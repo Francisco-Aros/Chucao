e284a4917ae4fe01ab0bcf0c240e715eecd4ba06
Francisco-Aros <franciscoarosmunoz@gmail.com>
2023-04-29 16:15:10 -0400

    Hace falta corregir los datos para la filtración

--- pages/2_Geolocalización_de_sitios.py
+++ pages/2_Geolocalización_de_sitios.py
@@ -22,7 +22,11 @@ ciudad["Fecha_de_Proceso"] = "27-04-2023"
 
 st.header("Desafío final")
 st.info("#### Visualización de geolocalización de incendios forestales en la Región de Los Lagos")
+#Hasta aquí se encuentra operativo el código a la fecha del 29-04-23
 
+
+#Se obtiene parte de la información contenida en excel para crear un nuevo DataFrame y se renombran las columnas para un mejor entendimiento.
+#Desde aquí vuelve a estar operativo a la fecha del 29-04-23
 geo_puntos_comuna = ciudad[["kingdom", "phylum", "class", "order", "family", "genus", "species", "scientificName", "decimalLatitude", "decimalLongitude", "eventDate", "day","month", "year", "basisOfRecord", "Fecha_de_Proceso"]].rename(columns={
     "kingdom": "Reino",
     "phylum": "Phylum",
@@ -47,6 +51,53 @@ geo_data["Fecha_de_Proceso"] = "08-11-2022"
 avg_lat = np.median(geo_data["Latitud"])
 avg_lng = np.median(geo_data["Longitud"])
 
+
+#Lo que viene a continuación es una nueva parte del código para ver si puede ser agregado a la visualización
+################################################################
+col_bar, col_pie, col_table = st.columns(3, gap="large")
+#Agrupar los datos en base a la columna donde están las comunas
+#Se gnera la serie de la agrupación usando "Size()"
+group_mes = geo_data.groupby(["Mes"]).size()
+#Se ordena de mayor a menor, gracias al uso del parámetro "ascending="
+group_mes.sort_values(axis="index", ascending=False, inplace=True)
+#Ya se pueden obtener los 5 primeros registros
+top5=group_mes[0:5]
+
+##############################################################
+st.info("#### Cantidad de incendios forestales por Comuna en la Región de Los Lagos")
+
+col_sel, col_map = st.columns([1,2])
+
+#Crear grupos por cantidad de puntos
+group_200= group_mes.apply(lambda x: x if x <= 200 else None)
+group_300= group_mes.apply(lambda x: x if x > 201 and x <=300 else None)
+group_max= group_mes.apply(lambda x: x if x > 300 else None)
+
+with col_sel:
+    comunas_agrupadas = st.multiselect(
+        label="Filtrar por grupos de mes",
+        options=["Menos de 200 Puntos", "201 a 300 Puntos", "Más de 300 Puntos"],
+        help="Selecciona la agrupación a mostrar",
+        default=[]
+    )
+    st.write("")    #Con este comando "St.write("")" y dejandolo en blanco, puedo crear una línea imaginaría y realizar un salto dentro del recuadro.
+    st.write("En el mapa que se aprecia a contnuación, ustedes podrán ver la geolocaliación de los avistamientos del Chucao en la zona correspondiente a Chile.")
+filtrar = []
+
+if "Menos de 200 Puntos" in comunas_agrupadas:
+    filtrar = filtrar + group_200.index.tolist()
+
+if "201 a 300 Puntos" in comunas_agrupadas:
+    filtrar = filtrar + group_300.index.tolist()
+
+if "Más de 300 Puntos" in comunas_agrupadas:
+    filtrar = filtrar + group_max.index.tolist()
+#Hasta aquí es la nueva parte del código que se agrega para ver si puede ser visualizada
+
+
+print(comunas_agrupadas)
+
+
 puntos_mapa = pdk.Deck(
     map_style=None,
     initial_view_state=pdk.ViewState(
@@ -68,7 +119,7 @@ puntos_mapa = pdk.Deck(
             opacity=1,
             radius_scale=20,
             radius_min_pixels=3,
-            get_fill_color=["Provincia == 'Llanquihue' ? 200 : 10", "Provincia == 'Llanquihue' ? 0 : 100", 60, 230]
+            get_fill_color=["Mes == '9' ? 200 : 10", "Mes == '9' ? 0 : 100", 60, 230]
         ),
     ],
     tooltip={
@@ -81,7 +132,8 @@ puntos_mapa = pdk.Deck(
 
 
 
+with col_map:
+    st.write(puntos_mapa)
 
-st.write(puntos_mapa)
 
 st.write("Como podrán apreciar existen puntos geolocalizados en zonas del Océano Pácifico, esto es debido a un error en la toma de datos al momento de realizar el resgistro")
