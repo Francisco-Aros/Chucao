b57fc7eaa8b315e0a4ee98889c3250ecfa688ce5
Francisco-Aros <franciscoarosmunoz@gmail.com>
2023-05-02 22:37:37 -0400

    Corrección de texto, no de código

--- Home.py
+++ Home.py
@@ -14,7 +14,7 @@ st.write("### Valdivia, Los Ríos")
 st.write("El motivo de esta web es poder geolocalizar los avistamientos del Scelorchilus rubecula, conocido popularmente como Chucao.")
 st.write("Tal como es señalado en la web de Aves de Chile, el Chucao presenta una distrubución desde el sur de Colchagua hasta Aysén")
 # Enlace a la página externa
-st.write("Para obtener más información sobre el Chucao, visita la página web de [Aves de Chile](https://www.avesdechile.cl/174.htm) en la cual podrán encontrar información detallada de esta ave.", allow_markdown=True)
+st.write("Para obtener más información sobre el Chucao, visita la página web de [Aves de Chile](https://www.avesdechile.cl/174.htm) en la cual podrán encontrar información detallada de esta y otras aves.", allow_markdown=True)
 
 #Con el siguiente código en vez de que aparezca el enlace dentro de la misma línea de código, se saltará una línea y creará un nuevo hipervinculo en la palabra que se desee. st.markdown("[Aves de Chile](https://www.avesdechile.cl/174.htm)")
 #Por otro lado, con el siguiente código, se creará una nueva línea en la visualización y aparecerá el enlace directo a la página web que se desea visitar. st.write('<a href="https://www.avesdechile.cl/174.htm" target="_blank">https://www.avesdechile.cl/174.htm</a>', unsafe_allow_html=True)
