e8c7864c2e25f79563427ebe4e54205a0908daff
Francisco-Aros <franciscoarosmunoz@gmail.com>
2023-05-02 21:03:25 -0400

    Home con hipervinculo funcional a página externa

--- Home.py
+++ Home.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+
 #Configuración inicial de la página
 st.set_page_config(
     page_icon=":thumbs_up:",
@@ -10,7 +11,15 @@ st.set_page_config(
 st.sidebar.write("Accesibilidad en la ciudad de Valdivia al año 2016")
 
 st.write("### Valdivia, Los Ríos")
-st.write("En esta página web usted podrá observar datos referentes al grado de accesibilidad en algunos de los principales puntos de la ciudad de Valdivia.")
+st.write("El motivo de esta web es poder geolocalizar los avistamientos del Scelorchilus rubecula, conocido popularmente como Chucao.")
+st.write("Tal como es señalado en la web de Aves de Chile, el Chucao presenta una distrubución desde el sur de Colchagua hasta Aysén")
+# Enlace a la página externa
+st.write("Para obtener más información sobre el Chucao, visita la página web de [Aves de Chile](https://www.avesdechile.cl/174.htm) en la cual podrán encontrar información detallada de esta ave.", allow_markdown=True)
+
+#Con el siguiente código en vez de que aparezca el enlace dentro de la misma línea de código, se saltará una línea y creará un nuevo hipervinculo en la palabra que se desee. st.markdown("[Aves de Chile](https://www.avesdechile.cl/174.htm)")
+#Por otro lado, con el siguiente código, se creará una nueva línea en la visualización y aparecerá el enlace directo a la página web que se desea visitar. st.write('<a href="https://www.avesdechile.cl/174.htm" target="_blank">https://www.avesdechile.cl/174.htm</a>', unsafe_allow_html=True)
+
+
 st.write("")
 st.write("Reportaje de 24 Horas, TVN")
 
